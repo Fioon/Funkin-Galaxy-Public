01f4e3875dc3b824f6155f7a0688fcc5512b5664
Fioon <135500399+Fioon@users.noreply.github.com>
2025-01-29 11:09:33 +0800

    Update AchievementsMenuState.hx

--- source/AchievementsMenuState.hx
+++ source/AchievementsMenuState.hx
@@ -71,6 +71,9 @@ class AchievementsMenuState extends MusicBeatState
 		add(descText);
 		changeSelection();
 
+		#if android
+                addVirtualPad(UP_DOWN, B);
+                #end
 		super.create();
 	}
 
